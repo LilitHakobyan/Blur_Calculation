112a959e3ce8bf42b3b03ba7048a77d34f446cea
Lilit <lilithakobyan1994@mail.ru>
2016-12-26 20:35:17 +0400

    file include from cmd

--- BitmapPicProj/BitmapPicProj/Program.cs
+++ BitmapPicProj/BitmapPicProj/Program.cs
@@ -2,17 +2,22 @@
 using System.Drawing;
 namespace Bitmap_project
 {
+  
     class Program
     {
 
         static void Main(string[] args)
         {
-            string bitmapFilePath = @"C:\1f48b.png";
+            string bitmapFilePath = @"E:\";
+            string bitmapFileName = Convert.ToString(Console.ReadLine());//@"C:\1f48b.png";
+            bitmapFilePath = bitmapFilePath + bitmapFileName;
             Bitmap b1 = new Bitmap(bitmapFilePath);
 
             int hight = b1.Height;
             int width = b1.Width;
+            Color myColor = Color.FromArgb(100, 150, 75);
 
+            // Color.FromArgb(BackColor.A, BackColor.R, BackColor.G, BackColor.B)
             Color[][] colorMatrix = new Color[width][];
             for (int i = 0; i < width; i++)
             {
@@ -20,6 +25,7 @@ namespace Bitmap_project
                 for (int j = 0; j < hight; j++)
                 {
                     colorMatrix[i][j] = b1.GetPixel(i, j);
+
                 }
             }
             // return colorMatrix;
@@ -29,21 +35,21 @@ namespace Bitmap_project
                 {
                     // Console.Write(colorMatrix[i][j]);
                     using (System.IO.StreamWriter file =
-                    new System.IO.StreamWriter(@"C:\Marix.txt", true))
+                    new System.IO.StreamWriter(@"E:\Marix.txt", true))
                     {
                         file.Write(colorMatrix[i][j]);
                     }
 
                 }
                 using (System.IO.StreamWriter file =
-                   new System.IO.StreamWriter(@"C:\Marix.txt", true))
+                   new System.IO.StreamWriter(@"E:\Marix.txt", true))
                 {
                     file.WriteLine("\n");
                 }
                 //Console.WriteLine("\n");
             }
-         
-            Console.ReadKey();
+
+            // Console.ReadKey();
 
         }
     }
