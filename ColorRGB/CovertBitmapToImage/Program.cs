fe740019ee05a1c96a9c895648c77db46073ae83
Lilit <lilithakobyan1994@mail.ru>
2017-04-10 19:29:44 +0400

    newproj

---
+++ ColorRGB/CovertBitmapToImage/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+namespace CovertBitmapToImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] imageData;
+
+            // Create the byte array.
+            var originalImage = Image.FromFile(@"E:\original1.bmp");
+               var ms = new MemoryStream();
+                originalImage.Save(ms, ImageFormat.Bmp);
+                imageData = ms.ToArray();
+            for (int i = 0; i < imageData.Length; i++)
+            {
+                Console.Write(imageData[i]+ " ");
+                
+            }
+             // Convert back to image.
+            var ms1 = new MemoryStream(imageData);
+            Image image = Image.FromStream(ms1);
+              image.Save(@"E:\newImage.bmp");
+
+            Console.ReadKey();
+
+        }
+    }
+}
--- ColorRGB/MatrixToImage/Program.cs
+++ ColorRGB/MatrixToImage/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 
 namespace MatrixToImage
@@ -18,11 +19,12 @@ namespace MatrixToImage
             StreamReader readerMatrix = File.OpenText("E://Sample.txt");
             string matrix = string.Empty;
             string element = string.Empty;
+            byte[] imageData=new byte[height*width];
             for (int i = 0; i < height; i++)
             {
                 matrix = readerMatrix.ReadLine();
-               char[] y= matrix.ToCharArray();
-               // Console.WriteLine(matrix);
+                char[] y = matrix.ToCharArray();
+                // Console.WriteLine(matrix);
                 int k = 0;
                 for (int j = 0; j < width; j++)
                 {
@@ -34,25 +36,27 @@ namespace MatrixToImage
                         k++;
                     }
                     k++;
-                    
+                    for (int s = 0; s < height*width; s++)
+                    {
+                        imageData[s] = Convert.ToByte(element);
+                    }
                     colorMatrix[i, j] = Convert.ToInt32(element);
                     element = string.Empty;
                 }
 
 
             }
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    Console.Write(colorMatrix[i, j] + ' ');
-                }
-                Console.WriteLine();
-            }
 
-            // Bitmap b1 = new Bitmap("E://Sample.txt");
+             //byte[] imageData = File.ReadAllBytes("E://Sample.txt");
+                var ms = new MemoryStream(imageData);
+            
+                Image image = Image.FromStream(ms);
+                image.Save(@"E:\newImage1.jpg");
+            
+            //http://stackoverflow.com/questions/21555394/how-to-create-bitmap-from-byte-array
+          
 
-
+           
             //  Console.WriteLine(colorMatrix);
             Console.ReadKey();
         }
